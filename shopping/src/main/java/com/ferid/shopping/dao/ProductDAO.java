1862dd4ced6aa82df1984c4981392879e7ff47fe
Ferid <58980432+Ferid2028@users.noreply.github.com>
2020-02-20 19:51:51 +0400

    find partial

--- shopping/src/main/java/com/ferid/shopping/dao/ProductDAO.java
+++ shopping/src/main/java/com/ferid/shopping/dao/ProductDAO.java
@@ -3,9 +3,15 @@ package com.ferid.shopping.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ferid.shopping.model.Product;
 
 public interface ProductDAO extends JpaRepository<Product, Integer> {
 public List<Product> findAllByUsername(String username);
+
+@Query(value="select * from product where name like %?3% limit ?1,?2",nativeQuery=true)
+public List<Product> findPartial(Integer begin,Integer length,String search);
+
+
 }
