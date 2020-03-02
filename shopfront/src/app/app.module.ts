fa4913b8487739285f5e354251b061925c657952
Ferid <58980432+Ferid2028@users.noreply.github.com>
2020-03-02 15:09:48 +0400

    imports
    terminal kodlar islemedi

--- shopfront/src/app/app.module.ts
+++ shopfront/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatInputModule, MatDialogModule, MatProgressSpinnerModule, MatButtonModule} from '@angular/material';
+import { FormsModule} from '@angular/forms';
+import { ConfirmationPopover Module } from 'angular-confirmation-popover';
 @NgModule({
   declarations: [
     AppComponent
@@ -16,10 +18,15 @@ import { MatInputModule, MatDialogModule, MatProgressSpinnerModule, MatButtonMod
     MatDialogModule, 
     MatProgressSpinnerModule, 
     MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ConfirmationPopoverModule.forRoot({
+      confirmButtonType:'succes',cancelButtonType:'danger',
+      confirmText:'Tesdiq',cancelText:'Legv'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents:[MaterialTestPageComponent]
+  entryComponents:[ ]
 })
 export class AppModule { }
