6d11beaa7766a5676f6f683dcbf15f13cad631a8
Ferid <58980432+Ferid2028@users.noreply.github.com>
2020-03-05 18:55:07 +0400

    Create model.ts

---
+++ shopfront/src/app/model/model.ts
@@ -0,0 +1,38 @@
+import { CustomerPageComponent } from '../component/customer-page/customer-page.component';
+
+
+export class Customer{
+    public id:number;
+    public name:string;
+    public adress:string;
+    public phone:string;
+    public orders:OrderModel[];
+}
+
+export class User{
+    public username:string;
+    public password:string;
+}
+
+
+export class Product{
+    public id:number;
+    public name:string;
+    public price:number;
+    public image:string;
+}
+
+export class OrderModel{
+    public id:number;
+    public totalprice:number;
+    public note:string;
+    public orderProducts:OrderProduct[];
+     public customer:Customer;
+
+}
+
+export class OrderProduct {
+    public id:number;
+    public product:Product;
+    public count:number;
+}
