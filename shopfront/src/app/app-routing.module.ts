f42a35727034b8afbec4f83fa7670a8cd7be247d
Ferid <58980432+Ferid2028@users.noreply.github.com>
2020-03-05 18:09:36 +0400

    Update app-routing.module.ts

--- shopfront/src/app/app-routing.module.ts
+++ shopfront/src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { LogoutComponent } from './component/logout/logout.component';
+import { ProductsComponent } from './component/products/products.component';
+import { AddProductComponent } from './component/add-product/add-product.component';
+import { CustomerPageComponent } from './component/customer-page/customer-page.component';
+import { OrdersComponent } from './component/orders/orders.component';
+import { OrderConfirmationComponent } from './component/order-confirmation/order-confirmation.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'login',component:LoginComponent},
+  {path:'logout',component:LogoutComponent},
+  {path:'products',component:ProductsComponent},
+  {path:'add-products',component:AddProductComponent},
+  {path:'customer-page',component:CustomerPageComponent},
+  {path:'orders',component:OrdersComponent},
+  {path:'order-confirm',component:OrderConfirmationComponent},
+  {path:'**',component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
