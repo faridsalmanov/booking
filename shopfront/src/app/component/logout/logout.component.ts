1f602031ea039cc42cd53e173888770a04d0b51f
Ferid <58980432+Ferid2028@users.noreply.github.com>
2020-03-09 17:25:27 +0400

    Update logout.component.ts

--- shopfront/src/app/component/logout/logout.component.ts
+++ shopfront/src/app/component/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-logout',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userS: UserService) { }
 
   ngOnInit(): void {
+    this.userS.token = '';
   }
 
 }
