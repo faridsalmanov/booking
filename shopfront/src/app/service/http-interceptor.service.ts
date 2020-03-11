a38f9f19adfc35edef1c372562bfee97dc53224a
Ferid <58980432+Ferid2028@users.noreply.github.com>
2020-03-11 09:20:57 +0400

    Update http-interceptor.service.ts

--- shopfront/src/app/service/http-interceptor.service.ts
+++ shopfront/src/app/service/http-interceptor.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { UserService } from './user.service';
+import { HttpRequest, HttpHandler } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HttpInterceptorService {
+export class HttpInterceptorService implements HttpInterceptorService {
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    let token: string = this.userS.token;
+    if (token == '') { } else {
 
-  constructor() { }
+      request = request.clone(
+
+        {
+          setHeaders: {
+            Authorization: token
+          }
+
+        }
+
+      );
+    }
+    return next.handle(request);
+  }
+
+  constructor(private userS: UserService) { }
 }
+
+
